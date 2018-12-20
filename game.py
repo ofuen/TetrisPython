d34f93f1fc1a9253ce0d1fe4e9d3561d20a24242
oscar <oscar@o2webdev.com>
2018-12-19 19:23:25 -0500

    :snake:

--- game.py
+++ game.py
@@ -127,3 +127,28 @@ T = [['.....',
 shapes = [S, Z, I, O, J, L, T]
 shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (0, 0, 255), (128, 0, 128)]
 # index 0 - 6 represent shape
+
+
+class Piece(object):
+      rows = 20 # y
+      columns = 10 # x
+
+      def __init__(self, column, row, shape):
+            self.x = column
+            self.y = row
+            self.shape = shape
+            self.color = shape_colors[shapes.index(shape)]
+            self.rotation = 0 # number from 0-3
+
+
+def create_grid(locked_positions={}):
+      grid = [[(0,0,0) for x in range(10)] for x in range(20)]
+
+      for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                  if (j,i) in locked_positions:
+                        c = locked_positions[(j,i)]
+                        grid[i][j] = c
+      return grid
+
+
